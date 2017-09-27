a1ed3b3050e2715d87f91de9ee52b4eb14d76ee6
Milosz Krysiak <s15194@lab2l-10.pjwstk.edu.pl>
2017-09-27 13:02:21 +0200

    add nick to program

--- src/gitdemo/Program.java
+++ src/gitdemo/Program.java
@@ -3,8 +3,7 @@ package gitdemo;
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("cos nowego");
+		System.out.println("s15194");
 	}
 
 }
