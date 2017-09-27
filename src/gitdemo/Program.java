8c7c286c64502348e5cc4988e7297e9469c2b393
WhiteWolf3500 <s15196@pjwstk.edu.pl>
2017-09-27 13:26:26 +0200

    Revert "Feature/s16019/dev2"

--- src/gitdemo/Program.java
+++ src/gitdemo/Program.java
@@ -8,13 +8,19 @@ public class Program {
 
 
 		System.out.println("s15194");
-		System.out.println("1v1 me in rocket league scrub");
+		// TODO Auto-generated method stub
+
 		System.out.println("SzymonPJWSTK");
+
 		System.out.println("s15722");
+		
 		System.out.println("Jaki kod ?");
 		System.out.println("s15664");
     System.out.println("hehe");
 		System.out.println("pjmpr");
+
+
+
 	}
 
 }
