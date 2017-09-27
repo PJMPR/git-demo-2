6f4e55c939bde722ca44456739fe397c2473b8ab
Beeert <beert13@o2.pl>
2017-09-27 13:04:26 +0200

    new class 2

--- src/gitdemo/Program.java
+++ src/gitdemo/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("cos nowego");
+		System.out.println("1v1 me in rocket league scrub");
 	}
 
 }
